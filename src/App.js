a7b4fafca42528844449bba009e23899090fcef3
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-15 09:49:43 +0300

    ekrana hangi componentlerin basılacağı belirlendi.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { memo, Suspense } from 'react'
+import 'typeface-roboto'
+import './App.css'
+import styled from 'styled-components'
+import { RecoilRoot } from 'recoil/dist'
+import AnketLoader from "./envanter-rapor/loader/AnketLoader";
+import IdariKayitLoader from "./envanter-rapor/loader/IdariKayitLoader";
+import UrunLoader from "./envanter-rapor/loader/UrunLoader";
+import BultenLoader from "./envanter-rapor/loader/BultenLoader";
+import ReferansLoader from "./envanter-rapor/loader/ReferansLoader";
+import BirimLoader from "./envanter-rapor/loader/BirimLoader";
+import Filtreler from "./envanter-rapor/filtreler/Filtreler";
+import Listeler from "./envanter-rapor/listeler/Listeler";
+import HeaderBar from "./envanter-rapor/HeaderBar";
+import {Colors} from "@blueprintjs/core";
+
+
+const Wrapper = styled.div`
+    padding: 0;
+`
+
+const YanMenu = styled.div`
+  width: 320px;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  position: fixed;
+  overflow-y: auto;
+  background-color: white;
+  border:1px solid ${Colors.LIGHT_GRAY3}
+`
+const OrtaBolme = styled.div`;
+  position: fixed;
+  width: 100%;
+  margin:20px 320px;
+  height: 100vh;
+  background-color:whitesmoke;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RecoilRoot>
+          <UrunLoader />
+          <BultenLoader />
+          <ReferansLoader />
+          <BirimLoader />
+          <AnketLoader/>
+          <IdariKayitLoader/>
+          <Wrapper>
+              <HeaderBar/>
+            <YanMenu>
+              <Filtreler />
+            </YanMenu>
+              <OrtaBolme>
+                  <Listeler/>
+              </OrtaBolme>
+          </Wrapper>
+      </RecoilRoot>
   );
 }
 
-export default App;
+export default memo(App);
