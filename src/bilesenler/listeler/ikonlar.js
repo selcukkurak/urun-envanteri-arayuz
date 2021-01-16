76b52653518aecd843a38a77bc2bec7b93839c8e
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2021-01-16 16:17:23 +0300

    ikonları düzelt

--- src/bilesenler/listeler/ikonlar.js
+++ src/bilesenler/listeler/ikonlar.js
@@ -1,15 +1,40 @@
-import React from 'react'
-import {Icon, Tooltip} from "@blueprintjs/core";
+import React, { memo } from 'react'
+import { Tag, Tooltip } from '@blueprintjs/core'
+import styled from 'styled-components'
 
+const Etiket = styled.div`
+  display: inline-block;
+  margin-right: 4px;
+`
 
-export const AnketIkon = props => (
-    <Tooltip content={"Anket"}>
-        <Icon icon={"confirm"}/>
+const Kaynaklar = styled.div`
+  display: inline-block;
+  margin-left: 12px;
+`
+
+const Metin = styled.div`
+  font-weight: 600;
+`
+
+export const Ikonlar = memo(props => {
+  const { sayilar, bultenler } = props
+
+  return (
+    <div>
+      {bultenler.length !== 0 && <Ikon tooltip='Haber Bülteni' intent='primary'><Metin>h</Metin></Ikon>}
+      <Kaynaklar>
+        {sayilar.anket !== 0 && <Ikon tooltip='Anket' intent='danger'><Metin>a</Metin></Ikon>}
+        {sayilar.idariKayit !== 0 && <Ikon tooltip='İdari Kayıt' intent='warning'><Metin>ik</Metin></Ikon>}
+        {sayilar.urunler !== 0 && <Ikon tooltip='İstatistiki Ürün' intent='success'><Metin>ü</Metin></Ikon>}
+      </Kaynaklar>
+    </div>
+  )
+})
+
+const Ikon = props => (
+  <Etiket>
+    <Tooltip content={props.tooltip}>
+      <Tag minimal intent={props.intent}>{props.children}</Tag>
     </Tooltip>
-)
-
-export const IdariKayitIkon = props => (
-    <Tooltip content="İdari Kayıt">
-        <Icon icon={"confirm"}/>
-    </Tooltip>
+  </Etiket>
 )
