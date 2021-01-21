6526b79332de7a7d5358e66c3af3c65fb3f0175d
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-21 10:04:38 +0300

    filtreli anketler sayısı gösteriildi.

--- src/bilesenler/listeler/Anket.js
+++ src/bilesenler/listeler/Anket.js
@@ -2,17 +2,15 @@ import React, { memo } from 'react'
 import { useRecoilValue } from 'recoil'
 import { siraliAnketler } from '../store/selectors'
 import Gosterge from './Gosterge'
+import useFiltreliAnketler from './hook/useFiltreliAnketler'
 
 function Anket (props) {
   const anketler = useRecoilValue(siraliAnketler)
-  let toplam = 0;
-  const anketToplam = props.sayilar.map((sayi)=>toplam+=Number(sayi.anket));
+  const filtreliAnketler = useFiltreliAnketler(props.filteriUrunler)
   return (
     <Gosterge
       toplam={anketler}
-      filtreli={anketToplam[anketToplam.length-1]}
-      filtreliText="Ürünlerin İçerdiği"
-      toplamText="Toplam"
+      filtreli={filtreliAnketler}
       baslik="Anket"
     />
 
