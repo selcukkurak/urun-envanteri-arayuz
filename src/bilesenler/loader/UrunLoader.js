e089b56f6ab9486e32a7569c7bb498cc01d7293d
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-21 10:07:12 +0300

    ürün içine idarikayıt ve anket idleri eklendi.

--- src/bilesenler/loader/UrunLoader.js
+++ src/bilesenler/loader/UrunLoader.js
@@ -10,17 +10,24 @@ async function urunleriGetir () {
   const kaynakKurumlarReq = Axios.get('/api/urunler/kaynak-kurumlar')
   const paylasimlarReq = Axios.get("/api/urunler/kuruluslar")
   const baglantilarReq = Axios.get('/api/urunler/baglantilar')
-  const [urunler, sayilar, kurumlar, paylasimlar, baglantilar] = await Axios.all([urunlerReq, sayilarReq, kaynakKurumlarReq, paylasimlarReq, baglantilarReq])
+  const idariKayitlarReq = Axios.get('/api/urunler/idari-kayitlar')
+  const anketlerReq = Axios.get('/api/urunler/anketler')
+  const [urunler, sayilar, kurumlar, paylasimlar, baglantilar, idariKayitlar, anketler] = await Axios
+      .all([urunlerReq, sayilarReq, kaynakKurumlarReq, paylasimlarReq, baglantilarReq, idariKayitlarReq, anketlerReq])
 
   const sayilarById = keyBy(sayilar.data, 'id')
   const kurumlarById = keyBy(kurumlar.data, 'id')
   const kuruluslarById = keyBy(paylasimlar.data, 'id')
   const baglantilarById = keyBy(baglantilar.data, 'id')
+  const idariKayitlarById = keyBy(idariKayitlar.data, 'id')
+  const anketlerById = keyBy(anketler.data, 'id')
 
   return urunler.data
     .filter(urun => !urun.taslak)
     .map(urun => ({
       ...urun,
+      anketler:anketlerById[urun.id].anketIdler,
+      idariKayitlar:idariKayitlarById[urun.id].idariKayitIdler,
       urunler: baglantilarById[urun.id].urunler,
       sayilar: sayilarById[urun.id],
       kurumlar: kurumlarById[urun.id].kurumlar,
