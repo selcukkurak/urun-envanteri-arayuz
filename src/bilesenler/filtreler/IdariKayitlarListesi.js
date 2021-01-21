aded4888507078421535514fc5d421eb5b179c32
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-21 10:20:58 +0300

    filtreli idari kayitlar basıldı.

--- src/bilesenler/filtreler/IdariKayitlarListesi.js
+++ src/bilesenler/filtreler/IdariKayitlarListesi.js
@@ -1,22 +1,23 @@
 import React, { memo, useCallback } from 'react'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { siraliIdariKayitlar } from '../store/selectors'
+import { useSetRecoilState } from 'recoil'
 import { seciliIdariKayitState } from '../store'
 import Select from 'react-select'
 import Filtre from './Filtre'
+import useFiltreliIdariKayitlar from '../listeler/hook/useFiltreliIdariKayitlar'
 
-function IdariKayitlarListesi () {
-    const idariKayitlar = useRecoilValue(siraliIdariKayitlar)
+function IdariKayitlarListesi (props) {
+    const {filtreliUrunler} = props
+    const filtreliIdariKayitlar = useFiltreliIdariKayitlar(filtreliUrunler)
     const setSeciliIdariKayit = useSetRecoilState(seciliIdariKayitState)
 
-    console.debug(idariKayitlar)
+    console.debug(filtreliIdariKayitlar)
 
-    const idariKayitlarOption = idariKayitlar.map(idari => ({label:idari.adi, value:idari.id}))
+    const idariKayitlarOption = filtreliIdariKayitlar.map(idari => ({label:idari.adi, value:idari.id}))
     const handleChange = useCallback(selectedOption => {
         setSeciliIdariKayit(selectedOption)
     }, [setSeciliIdariKayit])
     return (
-        <Filtre etiket={idariKayitlar && idariKayitlar.length}>
+        <Filtre etiket={filtreliIdariKayitlar && filtreliIdariKayitlar.length}>
             <Select
               isClearable={true}
               placeholder='İdari Kayıtlar'
