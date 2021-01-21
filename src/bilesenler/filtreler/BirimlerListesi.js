bf704c8958e73bf61e2abbb7179896558fb33e8a
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-21 10:04:03 +0300

    filtreli birimler seçeneklere basıldı.

--- src/bilesenler/filtreler/BirimlerListesi.js
+++ src/bilesenler/filtreler/BirimlerListesi.js
@@ -1,29 +1,32 @@
 import React, { memo, useCallback } from 'react'
 import { seciliBirimlerState } from '../store'
-import { useRecoilValue, useSetRecoilState } from 'recoil'
-import { siraliUrunDaireleri } from '../store/selectors'
+import { useSetRecoilState } from 'recoil'
 import Select from 'react-select'
 import Filtre from './Filtre'
+import useFiltreliBirimler from '../listeler/hook/useFiltreliBirimler'
 
-function BirimlerListesi () {
-    const setSeciliBirimler = useSetRecoilState(seciliBirimlerState)
-    const daireler = useRecoilValue(siraliUrunDaireleri)
+function BirimlerListesi (props) {
+  const { filtreliUrunler } = props
+  const setSeciliBirimler = useSetRecoilState(seciliBirimlerState)
+  const filtreliDaireler = useFiltreliBirimler(filtreliUrunler)
 
-    const handleChange = useCallback((selectedOption) => {
-        setSeciliBirimler(selectedOption)
-    }, [setSeciliBirimler])
-    const optionDaireler = daireler.map(birim => ({label:birim.adi, value:birim.id}))
-    return (
-        <Filtre etiket={daireler && daireler.length}>
-            <Select
-              placeholder='Üretici Birimler'
-              isMulti
-              options={optionDaireler}
-              onChange={handleChange}
-            />
-        </Filtre>
+  const handleChange = useCallback((selectedOption) => {
+    if (selectedOption !== null) {
+      setSeciliBirimler(selectedOption.map(a => a.value))
+    } else setSeciliBirimler([])
+  }, [setSeciliBirimler])
+  const optionDaireler = filtreliDaireler.map(birim => ({ label: birim.adi, value: birim.id }))
+  return (
+    <Filtre etiket={filtreliDaireler && filtreliDaireler.length}>
+      <Select
+        placeholder='Üretici Birimler'
+        isMulti
+        options={optionDaireler}
+        onChange={handleChange}
+      />
+    </Filtre>
 
-    )
+  )
 }
 
 export default memo(BirimlerListesi)
