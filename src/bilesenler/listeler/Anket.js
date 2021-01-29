ff2b63f59d239a1de18db024712a17dbe81b6372
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-29 09:28:53 +0300

    gösterge kartlarının içerisinden detaya ulaşma kısmı düzeltildi.

--- src/bilesenler/listeler/Anket.js
+++ src/bilesenler/listeler/Anket.js
@@ -4,28 +4,46 @@ import { siraliAnketler } from '../store/selectors'
 import Gosterge from './Gosterge'
 import useFiltreliAnketler from './hook/useFiltreliAnketler'
 import { MenuItem } from '@blueprintjs/core'
-import AnketDetayDialog from '../detaylar/AnketDetayDialog'
 import { localSort } from '../util/sort'
-
+import handleModal from '../detaylar/handleModal'
+import AnketDetayDialog from '../detaylar/AnketDetayDialog'
 function Anket (props) {
   const anketler = useRecoilValue(siraliAnketler)
   const filtreliAnketler = localSort(useFiltreliAnketler(props.filteriUrunler), 'adi')
+  const [selectedItem, setSelectedItem] = React.useState(null)
+  const [
+    open,
+    handleClickOpenModal,
+    handleClickCloseModal
+  ] = handleModal();
+
+  const handleClickItem = (event,index) => {
+    setSelectedItem(index);
+  }
+
   return (
-    <Gosterge
-      title={"Anket"}
-      toplam={anketler}
-      filtreli={filtreliAnketler}
-      length={filtreliAnketler.length}
-      itemRenderer={(index, key) => {
-        const anket = filtreliAnketler[index]
-        return (
-          <MenuItem
-            key={key}
-            text={(<AnketDetayDialog anket={anket}/>)}
-          />
-        )
-      }}
-    />
+    <div>
+      <Gosterge
+        title={"Anket"}
+        toplam={anketler}
+        filtreli={filtreliAnketler}
+        length={filtreliAnketler.length}
+        itemRenderer={(index, key) => {
+          const anket = filtreliAnketler[index]
+          return (
+            <div onClick={(event) => handleClickItem(event, anket)}>
+              <MenuItem
+                key={key}
+                text={anket.adi}
+                onClick={handleClickOpenModal}
+              />
+            </div>
+          )
+        }}
+      />
+      <AnketDetayDialog anket={selectedItem} open={open} handleClickCloseModal={handleClickCloseModal}/>
+    </div>
+
 
   )
 }
--- src/bilesenler/listeler/IdariKayit.js
+++ src/bilesenler/listeler/IdariKayit.js
@@ -6,27 +6,44 @@ import useFiltreliIdariKayitlar from './hook/useFiltreliIdariKayitlar'
 import { MenuItem } from '@blueprintjs/core'
 import IdariKayitDialog from '../detaylar/IdariKayitDialog'
 import { localSort } from '../util/sort'
+import handleModal  from '../detaylar/handleModal'
 
 function IdariKayit (props) {
   const idariKayitlar = useRecoilValue(siraliIdariKayitlar)
   const filtreliIdariKayitlar = localSort(useFiltreliIdariKayitlar(props.filtreliUrunler), 'adi')
-  return (
-    <Gosterge
-      title={"İdari Kayıt"}
-      toplam={idariKayitlar}
-      filtreli={filtreliIdariKayitlar}
-      length={filtreliIdariKayitlar.length}
-      itemRenderer={(index, key) => {
-        const idariKayit = filtreliIdariKayitlar[index]
-        return (
-          <MenuItem
-            key={key}
-            text={(<IdariKayitDialog idariKayit={idariKayit}/>)}
-          />
-        )
-      }}
-    />
+  const [selectedItem, setSelectedItem] = React.useState(null)
 
+  const [
+    open,
+    handleClickOpenModal,
+    handleClickCloseModal
+  ] = handleModal()
+
+  const handleClickItem = (event,index) => {
+    setSelectedItem(index);
+  }
+  return (
+    <div>
+      <Gosterge
+        title={"İdari Kayıt"}
+        toplam={idariKayitlar}
+        filtreli={filtreliIdariKayitlar}
+        length={filtreliIdariKayitlar.length}
+        itemRenderer={(index, key) => {
+          const idariKayit = filtreliIdariKayitlar[index]
+          return (
+            <div onClick={(event) => handleClickItem(event, idariKayit)}>
+              <MenuItem
+                key={key}
+                text={idariKayit.adi}
+                onClick={handleClickOpenModal}
+              />
+            </div>
+          )
+        }}
+      />
+      <IdariKayitDialog idariKayit={selectedItem} open={open} handleClickCloseModal={handleClickCloseModal}/>
+    </div>
   )
 }
 
