5baf9b19b0bd0fc015bb4d31618e2538bce853b0
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-29 10:42:43 +0300

    idari kayitlarin yanında kaynak kurumların gösterilmesi.

--- src/bilesenler/detaylar/DetayIdariKayitListesi.js
+++ src/bilesenler/detaylar/DetayIdariKayitListesi.js
@@ -3,6 +3,10 @@ import IdariKayitDialog from "./IdariKayitDialog";
 import { Col, Container, Row } from 'react-grid-system'
 import styled from 'styled-components'
 import   handleModal  from './handleModal'
+import { Tag } from '@blueprintjs/core'
+import { useRecoilValue } from 'recoil'
+import { siraliKurumlar } from '../store/selectors'
+import { seciliUrunState } from '../store'
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,7 +17,10 @@ const LeftElement = styled.div`
   flex: 1;
 `
 
-
+const Etiket = styled.div`
+  margin-left: 12px;
+  text-align: right;
+`
 
 export default function DetayIdariKayitListesi(props){
     const [
@@ -22,7 +29,12 @@ export default function DetayIdariKayitListesi(props){
       handleClickCloseModal
     ] = handleModal()
     const [selectedItem, setSelectedItem] = React.useState(null)
-
+    const kurumlar = useRecoilValue(siraliKurumlar)
+    const kaynakKurumId = props.idariKayitlar.map(idari => idari.kaynakKurumId)
+    const kurum =  kaynakKurumId.map(id => {
+      return kurumlar.find(k => k.kodu === id)
+    })
+    const seciliUrun = useRecoilValue(seciliUrunState)
     const handleClickItem = (event,index) => {
       setSelectedItem(index);
     }
@@ -30,8 +42,8 @@ export default function DetayIdariKayitListesi(props){
 
     return(
         <div>
-            {props.idariKayitlar.map(idari => (
-                <div key={idari.id} onClick={(event) => handleClickItem(event, idari)}>
+            {props.idariKayitlar.map((idari, index) => (
+                <div key={index} onClick={(event) => handleClickItem(event, idari)}>
                     <Container onClick={handleClickOpenModal}>
                         <Row>
                             <Col sm={12} style={{paddingTop:5, color:'#5A6F7B',cursor:'pointer', textDecoration:'underline',fontSize:'0.9em'}}>
@@ -39,6 +51,16 @@ export default function DetayIdariKayitListesi(props){
                                     <LeftElement>
                                         {idari.adi}
                                     </LeftElement>
+                                  {seciliUrun && kurum.map((k, key) => {
+                                    if(k.kodu === idari.kaynakKurumId && index === key){
+                                      return(
+                                        <Etiket>
+                                          <Tag>{k.adi}</Tag>
+                                        </Etiket>
+                                      )
+                                    }
+                                    else return null
+                                  })}
                                 </Wrapper>
                             </Col>
                         </Row>
