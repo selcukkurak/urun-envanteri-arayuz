a914556ffbba78edf09a118f514b0aa7487119f4
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-29 09:34:02 +0300

    value değerini ekledik.

--- src/bilesenler/listeler/Arama.js
+++ src/bilesenler/listeler/Arama.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { InputGroup } from '@blueprintjs/core'
 
 export default function Arama(props){
+
     return(
         <InputGroup
             placeholder="Ürünler içerisinde arayın..."
+            value={props.arananUrun}
             onChange={props.onUrunAramaChange}
             leftIcon={"search"}
         />
