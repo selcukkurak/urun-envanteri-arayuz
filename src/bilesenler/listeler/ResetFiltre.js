22f28c21749cd001af750e5d581bafb860de9d97
Selçuk Kurak <slckkrk1996@gmail.com>
2021-01-29 10:54:32 +0300

    herhangi bir filtre seçili ise buton gösterilsin şeklinde düzenlensin.

--- src/bilesenler/listeler/ResetFiltre.js
+++ src/bilesenler/listeler/ResetFiltre.js
@@ -1,4 +1,4 @@
-import {useSetRecoilState } from 'recoil'
+import {useRecoilState } from 'recoil'
 import {
   seciliAnketState, seciliBirimlerState, seciliBultenState, seciliCografiDuzeylerState, seciliHaberBulteniDurumuState,
   seciliIdariKayitState,
@@ -9,16 +9,16 @@ import {
 import { Button } from '@blueprintjs/core'
 
 export default function ResetFiltre(props){
-  const setSeciliUrun = useSetRecoilState(seciliUrunState)
-  const setSelectedKaynakKurum = useSetRecoilState(seciliKaynakKurumState)
-  const setSeciliAnket = useSetRecoilState(seciliAnketState)
-  const setSeciliIdariKayit = useSetRecoilState(seciliIdariKayitState)
-  const setSelectedKurulus = useSetRecoilState(seciliKaynakKurulusState)
-  const setSeciliBulten = useSetRecoilState(seciliBultenState)
-  const setSeciliHaberBulteniDurumu = useSetRecoilState(seciliHaberBulteniDurumuState)
-  const setSeciliCografiDuzeyler = useSetRecoilState(seciliCografiDuzeylerState)
-  const setSeciliBirimler = useSetRecoilState(seciliBirimlerState)
-  const setUretimSikliklar = useSetRecoilState(seciliUretimSikliklariState)
+  const [seciliUrun, setSeciliUrun] = useRecoilState(seciliUrunState)
+  const [seciliKurum, setSelectedKaynakKurum] = useRecoilState(seciliKaynakKurumState)
+  const [seciliAnket ,setSeciliAnket] = useRecoilState(seciliAnketState)
+  const [seciliKayit, setSeciliIdariKayit] = useRecoilState(seciliIdariKayitState)
+  const [seciliKurulus, setSelectedKurulus] = useRecoilState(seciliKaynakKurulusState)
+  const [seciliBulten, setSeciliBulten] = useRecoilState(seciliBultenState)
+  const [seciliHaberBulteniDurumu, setSeciliHaberBulteniDurumu] = useRecoilState(seciliHaberBulteniDurumuState)
+  const [seciliCografi, setSeciliCografiDuzeyler] = useRecoilState(seciliCografiDuzeylerState)
+  const [seciliBirimler, setSeciliBirimler] = useRecoilState(seciliBirimlerState)
+  const [seciliUretim, setUretimSikliklar] = useRecoilState(seciliUretimSikliklariState)
 
   const resetFiltre = () => {
     setSelectedKaynakKurum(null);
@@ -34,6 +34,9 @@ export default function ResetFiltre(props){
     props.removeUrunAramaChange();
   }
 
+  if(!seciliUrun && !seciliKurum && !seciliAnket && !seciliKayit && !seciliKurulus && !seciliBulten
+    && !seciliHaberBulteniDurumu && seciliCografi.length === 0 && seciliBirimler.length === 0 && seciliUretim.length === 0
+  ) return null
   return(
     <Button
       minimal
