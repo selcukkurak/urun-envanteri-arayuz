beab51a56e2758192e0cd0257b4fa7cec7779e58
Hasan TEKGÜL <hasan.tekgul@tuik.gov.tr>
2021-02-01 16:56:38 +0300

    sürüm ve daire eklendi

--- src/bilesenler/HeaderBar.js
+++ src/bilesenler/HeaderBar.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import logo from './images/tuik-logo.svg';
-import {Alignment, Navbar} from "@blueprintjs/core";
+import { Alignment, Classes, Navbar } from '@blueprintjs/core'
 import { ArayuzRenkleri } from '@tuik/renkler'
 
 export default function HeaderBar() {
   return (
-    <Navbar fixedToTop style={{ backgroundColor: ArayuzRenkleri.ustMenu }}>
+    <Navbar fixedToTop style={{ backgroundColor: ArayuzRenkleri.ustMenu }} className={Classes.DARK}>
       <Navbar.Group align={Alignment.LEFT}>
         <img src={logo} width="40px" alt="banner" />
         <Navbar.Divider/>
-        <Navbar.Heading style={{ color:"white" }}>İstatistiki Ürün Envanteri</Navbar.Heading>
+        <Navbar.Heading>İstatistiki Ürün Envanteri</Navbar.Heading>
+      </Navbar.Group>
+      <Navbar.Group align='right'>
+        <div>Metaveri ve Kayıt Sistemleri Daire Başkanlığı</div>
+        <Navbar.Divider/>
+        <div className={Classes.TEXT_MUTED}>{process.env.REACT_APP_SURUM}</div>
       </Navbar.Group>
     </Navbar>
   );
