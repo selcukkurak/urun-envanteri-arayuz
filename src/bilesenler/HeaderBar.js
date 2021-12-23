42e09144f03bc827b586361be72c86acbaf2d621
selcuk kurak <slckkrk1996@gmail.com>
2021-12-23 13:25:34 +0300

    engines alanı eklendi.

--- src/bilesenler/HeaderBar.js
+++ src/bilesenler/HeaderBar.js
@@ -3,14 +3,14 @@ import { Alignment, Classes, Navbar } from '@blueprintjs/core'
 
 export default function HeaderBar() {
   return (
-    <Navbar fixedToTop style={{ backgroundColor: "darkgray" }} className={Classes.DARK}>
+    <Navbar fixedToTop style={{ backgroundColor: "#013445" }} className={Classes.DARK}>
       <Navbar.Group align={Alignment.LEFT}>
         <img width="40px" alt="banner" />
         <Navbar.Divider/>
         <Navbar.Heading>İstatistiki Ürün Envanteri</Navbar.Heading>
       </Navbar.Group>
       <Navbar.Group align='right'>
-        <div>Metaveri ve Kayıt Sistemleri Daire Başkanlığı</div>
+        <div>- Daire Başkanlığı</div>
         <Navbar.Divider/>
         <div className={Classes.TEXT_MUTED}>{process.env.REACT_APP_SURUM}</div>
       </Navbar.Group>
